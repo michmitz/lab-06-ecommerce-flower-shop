26879fd1e2ae57fc67185c7d11f3fce067acf53e
Michelle Stermitz <stermitzm@gmail.com>
2020-07-14 14:26:15 -0700

    Added cart

---
+++ data/cart.js
@@ -0,0 +1,16 @@
+const cart = [{
+    id: 'rose',
+    quantity: 10
+}, {
+    id: 'lily',
+    quantity: 4
+}, {
+    id: 'snapdragon',
+    quantity: 7
+}, {
+    id: 'hydrangea',
+    quantity: 3
+}
+];
+
+export default cart;
